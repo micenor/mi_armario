33fe363e7cb89cdba51c5037a1280a08c51410d4
angelcriber99 <angelcriber99@gmail.com>
2023-06-06 15:34:50 +0200

    Initial commit

---
+++ app/src/main/java/com/angel/mi_armario/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.angel.mi_armario
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
