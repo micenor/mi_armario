e7cbfccda3654a65e2db4f3f9583765d0d78e73b
angelcriber99 <angelcriber99@gmail.com>
2023-06-07 15:50:44 +0200

    Icono, Nombre creados

--- app/src/main/java/com/angel/mi_armario/Splash.kt
+++ app/src/main/java/com/angel/mi_armario/Splash.kt
@@ -1,11 +1,22 @@
 package com.angel.mi_armario
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class Splash : AppCompatActivity() {
+
+    private val tiempo:Long = 2000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+                startActivity(Intent(this, PantallaPrincipal::class.java))
+            finish()
+        }, tiempo)
     }
+
 }
