29f0420b30618ff0c07ce2f2a18584e926dd8d25
angelcriber99 <angelcriber99@gmail.com>
2023-06-08 13:32:37 +0200

    splash mejorado. Animación hecha

--- app/src/main/java/com/angel/mi_armario/Splash.kt
+++ app/src/main/java/com/angel/mi_armario/Splash.kt
@@ -3,20 +3,20 @@ package com.angel.mi_armario
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
+import android.os.Looper
+import java.util.logging.Handler
 
 class Splash : AppCompatActivity() {
-
-    private val tiempo:Long = 2000
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
-                startActivity(Intent(this, PantallaPrincipal::class.java))
-            finish()
-        }, tiempo)
-    }
+        android.os.Handler(Looper.getMainLooper()).postDelayed({
 
+            val intent = Intent(this, PantallaPrincipal::class.java)
+            startActivity(intent)
+            finish()
+        }, 3500)
+
+    }
 }
